fa36bb51e80705cf07110f63efa57a12e95b6345
Edadul <edadul@uninorte.edu.co>
2024-05-05 02:07:13 -0500

    Frontend update
    Mejoras visuales y en la UI

--- Lab2_Estructura/Lab2_Estructura.py
+++ Lab2_Estructura/Lab2_Estructura.py
@@ -12,40 +12,98 @@ class MapState(rx.State):
     longitud: str = -74.78132
     mapa = folium.Map(location=(latitud, longitud))._repr_html_()
 
-    def change_cordinate(self, input):
-        latitud = input['input'].split(",")[0]
-        longitud = input['input'].split(",")[1]
+    def search_cordinate(self, input):
+        latitud = input['cordinate input'].split(',')[0]
+        longitud = input['cordinate input'].split(',')[1]
         self.latitud = latitud
         self.longitud = longitud
         self.mapa = folium.Map(location=(self.latitud, self.longitud))._repr_html_()
 
 def index() -> rx.Component:
-    return rx.vstack(
-        rx.heading("Airport Map", size="9", margin="12px",),
-        rx.divider(width="90%"),
-        rx.text("Enter a cordinate below with comma (lat, long)"),
-        rx.form.root(
-            rx.hstack(
-                rx.input(
-                    name="input",
-                    placeholder="Enter the cordinate here",
+    return rx.hstack(
+        rx.vstack(
+            rx.drawer.root(
+                rx.drawer.trigger(rx.button(rx.icon(tag='heart'),)),
+                rx.drawer.portal(
+                    rx.drawer.content(
+                        rx.vstack(
+                            rx.heading('Airport Map', size='8', margin_top='30px',margin_bottom='20px', margin_x='auto',),
+                            rx.divider(margin_bottom='25px',),
+                            rx.tabs.root(
+                                rx.center(
+                                    rx.tabs.list(
+                                        rx.tabs.trigger('Cordinates', value='tab1'),
+                                        rx.tabs.trigger('Airport codes', value='tab2'),
+                                    ),
+                                    margin_bottom='20px'
+                                ),
+                                rx.tabs.content(
+                                    rx.vstack(
+                                        rx.text('Enter a cordinate below', margin_x='auto'),
+                                        rx.form.root(
+                                            rx.vstack(
+                                                rx.input(
+                                                    name='cordinate input',
+                                                    placeholder='Ex: 10.96854, -74.78132',
+                                                    width='300px',
+                                                    height='40px'
+                                                ),
+                                                rx.button('Submit', type='submit', width='100%', height='40px'),
+                                            ),
+                                            margin_x='auto',
+                                            on_submit=MapState.search_cordinate,
+                                            reset_on_submit=True,
+                                        ),
+                                        rx.callout(
+                                            'Cordinate must have a comma between latitude and longitude',
+                                            icon='info',
+                                        ),
+                                        margin='9px'
+                                    ),
+                                    value='tab1',
+                                ),
+                                rx.tabs.content(
+                                    rx.vstack(
+                                        rx.text('Enter an airport code below', margin_top='9px'),
+                                        rx.form.root(
+                                            rx.hstack(
+                                                rx.input(
+                                                    name='airport code input',
+                                                    placeholder='Ex: 101301401',
+                                                ),
+                                                rx.button('Submit', type='submit'),
+                                                
+                                            ),
+                                        ),
+                                        rx.callout(
+                                            'Airport code must exist on the database :)',
+                                            icon='info',
+                                        ),
+                                        margin='9px'
+                                    ),
+                                    value='tab2',
+                                ),
+                                default_value='tab1'
+                            ),
+                            width='100%',
+                            aling='center',
+                        ),
+                        top='auto',
+                        height='100%',
+                        right='auto',
+                        width='25em',
+                        padding='2m',
+                        background_color='#FFF'
+                    ),
                 ),
-                rx.button("Submit", type="submit"),
+                aling='center',
+                direction='left',
             ),
-            on_submit=MapState.change_cordinate,
-            reset_on_submit=True,
+            align='center',
+            width='4em',
         ),
-        rx.hstack(
-            rx.text(f"Latitud: {MapState.latitud}, Longitud: {MapState.longitud}")
-        ),
-        rx.box(
-            rx.html(MapState.mapa),
-            width="70%",
-            border_width="thick",
-            margin="12px",
-        ),
-        align="center",
-    )
+        rx.html(MapState.mapa, width='100%',),
+    ),
 
 app = rx.App()
 app.add_page(index)
